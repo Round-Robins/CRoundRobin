90caa1d07ac41732f3fc91bf0b0194ad22acf189
Lucas Brendel <lucasbrendel@gmail.com>
2016-08-16 22:04:40 -0500

    minor documentation

--- src/round_robin_config.h
+++ src/round_robin_config.h
@@ -2,14 +2,14 @@
 #ifndef ROUND_ROBIN_CONFIG_H_
 #define ROUND_ROBIN_CONFIG_H_
 
+/// Period of a single task cycle
 #define TASK_PERIOD_MS (1)
 
+/// Period of a cycle to calculate CPU loading
 #define CPU_LOADING_PERIOD_MS (1000)
 
-#define CALIBRATE_LOADING
-
 #ifndef NUMBER_OF_TASKS
-#define NUMBER_OF_TASKS (1)
+#define NUMBER_OF_TASKS (1) ///< Number of allowed tasks
 #endif
 
 //#define USE_IDLE_TASK
